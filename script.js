ea7745eb230b7f187ff018908886752512407073
ivy13534 <andhika080305@gmail.com>
2024-02-21 13:40:26 +0700

    Project Cek Tiket-1

---
+++ script.js
@@ -0,0 +1,22 @@
+alert(
+    'Aplikasi Cek Tiket!'
+); //Pesan Awal
+
+const nama = prompt("Masukkan Nama Anda!"); //Membuat Input Nama
+const umur = prompt("Masukkan Umur Anda!"); //Membuat Input Umur
+let Studio; //Variabel baru
+
+if (umur >= 13) { //Perbandingan jika Umur lebih atau sama dengan 13
+    Studio = prompt("Pilih Studio A, B, atau C"); //Input pilihan Studio
+    if (Studio == "A") { //Jika Studio A
+        alert(`Tiket ${nama}, Studio A, Umur ${umur}`);
+    } else if (Studio == "B"){ //Jika Studio B
+        alert(`Tiket ${nama}, Studio B, Umur ${umur}`);
+    } else if (Studio == "C"){ //Jika Studio C
+        alert(`Tiket ${nama}, Studio C, Umur ${umur}`);
+    } else { //Pilihan studio tidak ada
+        alert("Pilihan Studio tidak valid");
+    }
+} else { //Kondisi Umur kurang dari 13
+    alert (`Tidak boleh masuk`);
+}
